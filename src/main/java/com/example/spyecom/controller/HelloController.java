316d78695e4bb345e7452852975a9a71dc01f8e5
vuongdq <quocvuong_it@yahoo.com>
2022-08-25 00:08:49 +0700

    init project

--- src/main/java/com/example/spyecom/controller/HelloController.java
+++ src/main/java/com/example/spyecom/controller/HelloController.java
@@ -1,4 +1,14 @@
-package src.main.java.com.example.spyecom.controller;
 
+package com.example.spyecom.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/web")
 public class HelloController {
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
 }
