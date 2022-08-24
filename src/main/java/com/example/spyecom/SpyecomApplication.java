7ee92f06efeb62f7cdf66b368c5c3773816b1401
vuongdq <quocvuong_it@yahoo.com>
2022-08-24 23:45:53 +0700

    init project

---
+++ src/main/java/com/example/spyecom/SpyecomApplication.java
@@ -0,0 +1,13 @@
+package com.example.spyecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpyecomApplication.class, args);
+	}
+
+}
